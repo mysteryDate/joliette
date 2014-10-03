39663e29ac85849755b7331a8ba288ddf086a6fd
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-10-03 16:41:53 -0400

    Started write to file code

--- gmail_monitor/gmonitor.py
+++ gmail_monitor/gmonitor.py
@@ -114,36 +114,39 @@ class Message():
 		self.message = ""
 
 gmail = Monitor()
-gmail.populate()
-gmail.printDatabase()
-while True:
-	gmail.update()
-	# something changed
-	if gmail.recentThreads.has_key('history'):
-		modifiedMessages = set()
-		# Get unique ids for every changed message
-		for thread in gmail.recentThreads['history']:
-			if thread.has_key('messages'):
-				for message in thread['messages']:
-					modifiedMessages.add(message['id'])
-		for messageId in modifiedMessages:
-			# Check to see if it's moved to or from a filtered folder
-			if messageId in gmail.database:
-				print messageId, "\t|", gmail.database[messageId].message
-				messageData = gmail.service.users().messages().get(
-					userId='me', id=messageId, format='minimal').execute()
-				if any([_ for _ in messageData['labelIds'] if _ in FILTERED_LABELS]):
-					gmail.database[messageData['id']].active = False
-					print "Deactivated"
-				else:
-					gmail.database[messageData['id']].active = True
-					print "Activated"
-			# Then it must be new
-			else:
-				gmail.addMessageToDatabase(messageId)
-				print messageId, "\t|", gmail.database[messageId].message
-				print "Added"
-	time.sleep(2)
+
+def create_file
+
+# gmail.populate()
+# gmail.printDatabase()
+# while True:
+# 	gmail.update()
+# 	# something changed
+# 	if gmail.recentThreads.has_key('history'):
+# 		modifiedMessages = set()
+# 		# Get unique ids for every changed message
+# 		for thread in gmail.recentThreads['history']:
+# 			if thread.has_key('messages'):
+# 				for message in thread['messages']:
+# 					modifiedMessages.add(message['id'])
+# 		for messageId in modifiedMessages:
+# 			# Check to see if it's moved to or from a filtered folder
+# 			if messageId in gmail.database:
+# 				print messageId, "\t|", gmail.database[messageId].message
+# 				messageData = gmail.service.users().messages().get(
+# 					userId='me', id=messageId, format='minimal').execute()
+# 				if any([_ for _ in messageData['labelIds'] if _ in FILTERED_LABELS]):
+# 					gmail.database[messageData['id']].active = False
+# 					print "Deactivated"
+# 				else:
+# 					gmail.database[messageData['id']].active = True
+# 					print "Activated"
+# 			# Then it must be new
+# 			else:
+# 				gmail.addMessageToDatabase(messageId)
+# 				print messageId, "\t|", gmail.database[messageId].message
+# 				print "Added"
+# 	time.sleep(2)
 
 # "Label_3" = "Refuser"
 # "Label_5" = "RefuserAutomatique"
