a8a2454f425b577d5802275cc486338ee3e53992
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-10-06 16:18:15 -0400

    Everything is caught

---
+++ gmail_monitor/checker.py
@@ -0,0 +1,45 @@
+import gmonitor
+import time
+import pdb
+
+def main():
+    gmail = gmonitor.Monitor()
+
+    while(True):
+        gmail.update()
+        # something changed
+        if gmail.recentThreads.has_key('history'):
+            print "start"
+            modifiedMessages = set()
+            # Get unique ids for every changed message
+            for thread in gmail.recentThreads['history']:
+                if thread.has_key('messages'):
+                    for message in thread['messages']:
+                        modifiedMessages.add(message['id'])
+            for messageId in modifiedMessages:
+                # Check to see if it's moved to or from a filtered folder
+                if messageId in gmail.database:
+                    print messageId, "\t|", gmail.database[messageId].message
+                    try:
+                        messageData = gmail.service.users().messages().get(
+                            userId='me', id=messageId, format='minimal').execute()
+                        if any([_ for _ in messageData['labelIds'] if _ in gmail.FILTERED_LABELS]):
+                            gmail.database[messageData['id']].active = False
+                            print "Deactivated"
+                        else:
+                            gmail.database[messageData['id']].active = True
+                            print "Activated"
+                    except:
+                        # It was fully deleted
+                        del gmail.database[messageId]
+                # Then it must be new
+                else:
+                    gmail.add_message_to_database(messageId)
+                    try:
+                        print messageId, "\t|", gmail.database[messageId].message, "| Added"
+                    except:
+                        print messageId, " not found"
+            print "end"
+        time.sleep(0.5)
+
+main()
